138a7425212844dd7aa86074dfb7f8f76b21b700
Subham Sahu <115224258+subham13-02@users.noreply.github.com>
2023-07-01 17:07:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-//your JS code here. If required.
 // script.js
 
 window.onload = function() {
-  var colorSelect = document.getElementById("colorSelect");
   var removeButton = document.getElementById("removeButton");
 
   removeButton.addEventListener("click", function() {
+    var colorSelect = document.getElementById("colorSelect");
     var selectedIndex = colorSelect.selectedIndex;
     if (selectedIndex !== -1) {
-      colorSelect.remove(selectedIndex);
+      colorSelect.options[selectedIndex].remove();
     }
   });
 };
